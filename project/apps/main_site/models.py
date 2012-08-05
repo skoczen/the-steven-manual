ec88f2a3ffc703b1a3406d65b25080510ea01527
Steven Skoczen <steven@quantumimagery.com>
2012-08-04 23:24:09 -0700

    nicer admin

--- project/apps/main_site/models.py
+++ project/apps/main_site/models.py
@@ -8,14 +8,23 @@ class Emotion(models.Model):
     helpful = models.TextField(blank=True, null=True)
     updated_at = models.DateField(auto_now=True)
 
+    def __unicode__(self):
+        return "%s" % self.name
+
 class EmotionEvent(models.Model):
     emotion = models.ForeignKey(Emotion)
     date = models.DateField(auto_now_add=True)
     notes = models.TextField(blank=True, null=True)
     updated_at = models.DateField(auto_now=True)
 
+    def __unicode__(self):
+        return "%s at %s" % (self.emotion.name, self.date)
+
 
 class Value(models.Model):
     name = models.CharField(max_length=200)
     explanation = models.TextField(blank=True, null=True)
     updated_at = models.DateField(auto_now=True)
+
+    def __unicode__(self):
+        return "%s" % self.name
