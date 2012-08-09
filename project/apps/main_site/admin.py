4b46cba681414b809b568ec63adf7fa4583c9e7d
Steven Skoczen <steven@quantumimagery.com>
2012-08-09 09:03:25 -0700

    fixed admin typo

--- project/apps/main_site/admin.py
+++ project/apps/main_site/admin.py
@@ -3,7 +3,7 @@ from main_site.models import Emotion, EmotionEvent, Value, GutterBumper
     
 
 class GutterBumperOptions(admin.ModelAdmin):
-    list_display = ("date", "sleep_hrs","work_hrs","alone_hrs","friend_hrs","relationship_hrs","public_hours","off","worked_out","mediated","number_of_beers","presence","happiness","creativity","morning_mood")
+    list_display = ("date", "sleep_hrs","work_hrs","alone_hrs","friend_hrs","relationship_hrs","public_hrs","off","worked_out","mediated","number_of_beers","presence","happiness","creativity","morning_mood")
     list_filter = ("date",)
     # search_fields = ('name','description',)
     # exclude = ("slug",)
