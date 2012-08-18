e28dd8158eeb7bfcc8059165b7a6274c504e9d1f
Steven Skoczen <steven@quantumimagery.com>
2012-08-17 23:09:35 -0700

    added workday

--- project/apps/main_site/views.py
+++ project/apps/main_site/views.py
@@ -1,4 +1,5 @@
 import datetime
+import math
 from annoying.decorators import render_to, ajax_request
 from main_site.models import GutterBumper
 from main_site.forms import GutterBumperForm
@@ -26,6 +27,29 @@ def home(request):
 @render_to("main_site/monthly.html")
 def monthly(request):
     gutterbumpers = GutterBumper.objects.filter(date__gte=datetime.date.today()-datetime.timedelta(days=31))
+    total_days = gutterbumpers.count()
+    total_workdays = total_days - math.floor(total_days/7*2)
+    total_sleep = 0
+    total_work = 0
+    total_alone = 0
+    total_friend = 0
+    total_public = 0
+    total_relationship = 0
+    for g in gutterbumpers:
+        total_sleep += g.sleep_hrs or 0
+        total_work += g.work_hrs or 0
+        total_alone += g.alone_hrs or 0
+        total_friend += g.friend_hrs or 0
+        total_public += g.public_hrs or 0
+        total_relationship += g.relationship_hrs or 0
+
+    avg_sleep = total_sleep / total_days
+    avg_work = total_work / total_days
+    avg_alone = total_alone / total_days
+    avg_friend = total_friend / total_days
+    avg_public = total_public / total_days
+    avg_relationship = total_relationship / total_days
+    avg_work_per_workday = total_work / total_workdays
     return locals()
 
 @render_to("main_site/daily.html")
