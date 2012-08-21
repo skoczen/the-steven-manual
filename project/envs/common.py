819fe86dc5896e77c89f7b98390e2f916416a676
Steven Skoczen <steven@quantumimagery.com>
2012-08-20 22:37:25 -0700

    even better monthlies

--- project/envs/common.py
+++ project/envs/common.py
@@ -21,10 +21,10 @@ DEFAULT_FROM_EMAIL = SERVER_EMAIL
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'project.sqlite3',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': DB_PASSWORD,                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'thestevenmanual',                      # Or path to database file if using sqlite3.
+        'USER': 'skoczen',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
